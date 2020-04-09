b83300a85f22b99fecc96d34a418a6d9de222329
Nigel Gee <nigel.gee@live.com>
2020-04-09 16:28:18 +0100

    Initial Commit

---
+++ DirtyApron/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DirtyApron
+//
+//  Created by Nigel Gee on 09/04/2020.
+//  Copyright © 2020 Nigel Gee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
